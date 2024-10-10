036ae35895aa2ecea72956d745e413b94133fa8d
Jigisha Manohar <jigishamanohar18@gmail.com>
2024-10-11 01:11:12 +0530

    Add employee, dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,43 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
 
 import Register from './pages/Register/Register'
 import Login from './pages/Login/Login'
 import WebcamCapture from './pages/WebcamCapture/WebcamCapture'
+import Dashboard from './pages/Dashboard/Dashboard'
+import AddEmployee from './pages/AddEmployee/AddEmployee'
 
 function App() {
   return (
-    <div className='App'>
-      <Router>
-        <Routes>
-          <Route
-            path='/register'
-            Component={Register}
-          />
-          <Route
-            path='/login'
-            Component={Login}
-          />
-          <Route
-            path='/webcam'
-            Component={WebcamCapture}
-          />
-        </Routes>
-      </Router>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route
+              path="/register"
+              Component={Register}
+            />
+            <Route
+              path="/login"
+              Component={Login}
+            />
+            <Route
+              path="/webcam"
+              Component={WebcamCapture}
+            />
+            <Route
+              path="/dashboard"
+              Component={Dashboard}
+            />
+            <Route
+              path="/add_employee"
+              Component={AddEmployee}
+            />
+          </Routes>
+        </Router>
+      </div>
+    </ChakraProvider>
   )
 }
 
